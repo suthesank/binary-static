1a6a8c3e868bbde02e5c5635bfbc0d519705de23
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2020-11-12 15:25:26 +0800

    test interval loop

--- src/javascript/_common/base/livechat.js
+++ src/javascript/_common/base/livechat.js
@@ -4,7 +4,7 @@ const BinarySocket = require('./socket_base');
 const ClientBase = require('./client_base');
 
 const LiveChat = (() => {
-    // let logged_in = false;
+    let logged_in = false;
     const initial_session_variables = { loginid: '', landing_company_shortcode: '', currency: '', residence: '', email: '' };
     const init = () => {
         if (window.LiveChatWidget) {
@@ -24,41 +24,41 @@ const LiveChat = (() => {
             //     clientId : '66aa088aad5a414484c1fd1fa8a5ace7',
             // });
 
-            // const checkLoginStatus = () => {
-            //     if (ClientBase.isLoggedIn()) {
-            //         if (logged_in === false) {
-            //             const loginid = ClientBase.get('loginid');
-            //             const landing_company_shortcode = ClientBase.get('landing_company_shortcode');
-            //             const currency = ClientBase.get('currency');
-            //             const residence = ClientBase.get('residence');
-            //             const email = ClientBase.get('email');
+            const checkLoginStatus = () => {
+                if (ClientBase.isLoggedIn()) {
+                    if (logged_in === false) {
+                        // const loginid = ClientBase.get('loginid');
+                        // const landing_company_shortcode = ClientBase.get('landing_company_shortcode');
+                        // const currency = ClientBase.get('currency');
+                        // const residence = ClientBase.get('residence');
+                        // const email = ClientBase.get('email');
 
-            //             const client_session_variables = {
-            //                 ...loginid && { loginid },
-            //                 ...landing_company_shortcode && { landing_company_shortcode },
-            //                 ...currency && { currency },
-            //                 ...residence && { residence },
-            //                 ...email && { email },
-            //             };
+                        // const client_session_variables = {
+                        //     ...loginid && { loginid },
+                        //     ...landing_company_shortcode && { landing_company_shortcode },
+                        //     ...currency && { currency },
+                        //     ...residence && { residence },
+                        //     ...email && { email },
+                        // };
 
-            //             window.LiveChatWidget.call('set_session_variables', client_session_variables);
-            //             // do logged in stuffs
-            //         }
-            //         logged_in = true;
-            //     } else {
-            //         if (logged_in === true) {
-            //             if (window.LiveChatWidget.get('chat_data')) {
-            //                 const chatID = window.LiveChatWidget.get('chat_data').chatId;
-            //                 customerSDK.deactivateChat({ chatId: chatID });
-            //             }
-            //             window.LiveChatWidget.call('set_customer_email', ' ');
-            //             window.LiveChatWidget.call('set_customer_name', ' ');
-            //         }
-            //         logged_in = false;
-            //     }
-            // };
+                        // window.LiveChatWidget.call('set_session_variables', client_session_variables);
+                        // do logged in stuffs
+                    }
+                    logged_in = true;
+                } else {
+                    if (logged_in === true) {
+                        // if (window.LiveChatWidget.get('chat_data')) {
+                        //     const chatID = window.LiveChatWidget.get('chat_data').chatId;
+                        //     customerSDK.deactivateChat({ chatId: chatID });
+                        // }
+                        // window.LiveChatWidget.call('set_customer_email', ' ');
+                        // window.LiveChatWidget.call('set_customer_name', ' ');
+                    }
+                    logged_in = false;
+                }
+            };
 
-            // setInterval(checkLoginStatus, 500);
+            setInterval(checkLoginStatus, 500);
 
             window.LiveChatWidget.on('visibility_changed', ({ visibility }) => {
                 // only visible to CS
